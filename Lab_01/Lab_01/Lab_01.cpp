f0ccef76039e4310c70dd2479d9ae09e437863f8
KrissBar <kris.barton.01@bk.ru>
2025-09-30 15:08:26 +0200

    Add files via upload

---
+++ Lab_01/Lab_01/Lab_01.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a;  //принимает только целочисленные значение, стороны треугольника 
+    int b;
+    int c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    float p = (a + b + c) / 2.0;
+    float s = sqrt(p * ((p - a) * (p - b) * (p - c)));  //принимает вещественные числа, т.к в ходе решения может образоваться дробная часть
+
+    std::cout << "Значение p = " << p << std::endl;
+    std::cout << "Значение s = " << s << std::endl;
+
+    cout << "Минимальное значение int: " << -numeric_limits<int>::max() << endl;
+    cout << "Максимальное значение int: " << numeric_limits<int>::max() << endl;
+    cout << "Минимальное значение float: " << -numeric_limits<float>::max() << endl;
+    cout << "Максимальное значение float: " << numeric_limits<float>::max() << endl; 
+}
